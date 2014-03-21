49025185b77ffa9b47154d0885f01529b467e999
Jarno Nijboer <git@nijboer.me>
2014-03-21 08:45:58 +0100

    added views

--- client/views/groups/detail.js
+++ client/views/groups/detail.js
@@ -1,5 +1,22 @@
-Template['groups-detail'].helpers({
+Template['groupsDetail'].helpers({
+
 });
 
-Template['groups-detail'].events({
+Template['groupsDetail'].jarno = function () {
+	return Groups.find();
+		// var userId = Meteor.userId();
+
+		// return Groups.find({
+		// 	deleted: false,
+		// 	$or: [
+		// 		{ ownerId: userId },
+		// 		{ userIds: userId }
+		// 	],
+		// });
+	},
+
+Template['groupsDetail'].events({
+	'click #btnGroupSave': function (e) {
+
+	},
 });
--- client/views/groups/overview.js
+++ client/views/groups/overview.js
@@ -1,5 +1,19 @@
 Template['groups-overview'].helpers({
+	groups: function () {
+		var userId = Meteor.userId();
+
+		return Groups.find({
+			deleted: false,
+			$or: [
+				{ ownerId: userId },
+				{ userIds: userId }
+			],
+		});
+	},
 });
 
 Template['groups-overview'].events({
+	'click #btnGroupAdd': function (e) {
+		$('#dlgGroupDetail').modal('show');
+	},
 });
